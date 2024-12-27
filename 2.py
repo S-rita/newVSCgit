db6d8d7e8013a80b2f89bccc49998afbaf253d11
knppkp <140078295+knppkp@users.noreply.github.com>
2024-12-27 17:19:50 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -21,17 +21,26 @@ class Walk( Transportation ):
    def find_cost( self ):
       return 0
 
+class Boat( Transportation ):
+   def __init__( self, start, end, distance, pier ):
+      Transportation.__init__( self, start, end, distance)
+      self.pier = pier
 
+   def find_cost( self ):
+      return 15 * self.pier
    
 # main program
 
 travel_cost = 0
 
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         #Train("Ladkrabang Station","Payathai Station",40,6),
-         #Taxi("Payathai Station","The British Council",3) ]
+trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
+         Taxi("KMITL SCB Bank","Ladkrabang Station",5),
+         Train("Ladkrabang Station","Payathai Station",40,6),
+         Taxi("Payathai Station","The British Council",3) 
+         Walk("The British Council","Pinklao Pier",0.6),
+         Boat("Pinklao Pier", "Rajini Pier", 40, 4)
+      ]
 
 for travel in trip:
    travel_cost += travel.find_cost()
-print travel_cost
+print (travel_cost)
