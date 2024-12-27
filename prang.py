57517e58757ab90738ab91e6d316d7cdec3c1f34
S-rita <iiprangja@gmail.com>
2024-12-27 17:14:42 +0700

    prang

--- prang.py
+++ prang.py
@@ -7,26 +7,31 @@ class Simple_drawing_window_prang(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Github Drawing")
-        self.rabbit = QPixmap("images/rabbit.png")
+        self.cat = QPixmap("images/cat.png")
         
+
     def paintEvent(self, e):
         p = QPainter()
         p.begin(self)
-        
-        p.setPen(QColor(0,0,0))
-        p.setBrush(QColor(0, 127, 0))
-        p.drawPolygon([
-            QPoint(70, 100), QPoint(100, 110),
-            QPoint(130, 100), QPoint(100, 150),
-        ])
-        p.setPen(QColor(25, 127, 0))
+
+        p.setPen(QColor(0, 0, 0)) 
+        p.setBrush(QColor(0, 127, 255)) 
+        p.drawEllipse(50, 100, 150, 75)  
+
         p.setBrush(QColor(255, 127, 0))
-        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
+        p.drawPolygon([QPoint(200, 125), QPoint(250, 100), QPoint(200, 175)])
+
+        p.setBrush(QColor(100, 100, 255)) 
+        p.drawPolygon([QPoint(110, 120), QPoint(130, 80), QPoint(150, 120)]) 
+        p.drawPolygon([QPoint(120, 170), QPoint(140, 130), QPoint(160, 170)])  
+
+        p.setBrush(QColor(255, 255, 255))  
+        p.drawEllipse(110, 130, 20, 20) 
+        p.setBrush(QColor(0, 0, 0)) 
+        p.drawEllipse(115, 135, 10, 10) 
         
-        p.drawPolygon(
-            [QPoint(50, 200), QPoint(150, 200), QPoint(100, 400),]
-        )
-        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
+        p.drawPixmap(QRect(200, 100, 320, 320), self.cat)
+
         p.end()
             
 def main():
