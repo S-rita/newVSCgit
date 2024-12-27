da4bf506ac7076323e43477e05a8b731509582fe
S-rita <iiprangja@gmail.com>
2024-12-27 17:30:33 +0700

    prang

--- 2.py
+++ 2.py
@@ -24,7 +24,6 @@ class Taxi(Transportation):
    def __init__( self, start, end, distance ):
       Transportation.__init__( self, start, end, distance)
 
-
    def find_cost( self ):
       return self.distance * 40
    
