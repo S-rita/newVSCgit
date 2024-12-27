8ef9f60bee6b2a097cefeef04e6bcf17039e4d7d
phathompolllllllllllllllll <phathompols@gmail.com>
2024-12-27 17:09:54 +0700

    bew
    By bew

--- bew.py
+++ bew.py
@@ -8,15 +8,16 @@ class Simple_drawing_window(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple GitHub Drawing")
-        self.rabbit = QPixmap("imageBew/rabbit.jpg")
+        self.rabbit = QPixmap("images/rabbit.jpg")
 
     def paintEvent(self, e):
         p = QPainter()
         p.begin(self)
 
-        p.setPen(QColor(0, 100, 0))
-        p.setBrush(QColor(0, 145, 0))
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
         p.drawRect(100, 100, 25, 25)
+        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
 
 
